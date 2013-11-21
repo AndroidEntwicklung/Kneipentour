db35980829693120ba024246a0284b1220469304
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2013-11-21 15:35:10 +0100

    Fisrt steps of a custom list adapter

---
+++ src/com/example/kneipentour/KneipenItem.java
@@ -0,0 +1,47 @@
+package com.example.kneipentour;
+
+public class KneipenItem {
+private String name;
+private String street;
+private String distance;
+private String iconpath;
+
+public String getName()
+{
+	return name;
+}
+public void setName(String _name)
+{
+	this.name = _name;
+}
+public String getStreet()
+{
+	return street;
+}
+public void setStreet(String _street)
+{
+	this.street = _street;
+}
+public String getDistance()
+{
+	return distance;
+}
+public void setDistance(String _distance)
+{
+	this.distance = _distance;
+}
+public String getIconpath()
+{
+	return iconpath;
+}
+public void setIconpath(String _iconpath)
+{
+	this.iconpath = _iconpath;
+}
+
+@Override
+public String toString()
+{
+	return "[ name=" + name + ", street=" + street + ", distance=" + distance + ", iconpath=" + iconpath + "]";
+} 
+}
