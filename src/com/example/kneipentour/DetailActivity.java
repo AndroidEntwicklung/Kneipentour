de59f3f023ef33acb82d51eb3152f50b61d29b32
Fabian Gärtner <fabian.gaertner@hs-furtwangen.de>
2013-11-28 14:48:11 +0100

    Added Volley

--- src/com/example/kneipentour/DetailActivity.java
+++ src/com/example/kneipentour/DetailActivity.java
@@ -1,7 +1,22 @@
 package com.example.kneipentour;
 
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.util.LruCache;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailActivity extends Activity {
@@ -22,8 +37,40 @@ public class DetailActivity extends Activity {
         } else {
             selected_name = (String) savedInstanceState.getSerializable("selected_name");
         }
+        
+        
+        // get data       
+        Datenbank localDB = new Datenbank();
+		ArrayList<KneipenListItem> kneipe_details = localDB.getListData(selected_name, "", "");
+		KneipenListItem found_kneipe = kneipe_details.get(0);
 
-        TextView tview = (TextView) findViewById(R.id.textView);
-        tview.setText(selected_name);
+		// find views
+        TextView name_tv = (TextView) findViewById(R.id.name_tv);
+        TextView address_tv = (TextView) findViewById(R.id.address_tv);
+        TextView rating_tv = (TextView) findViewById(R.id.rating_tv);
+        TextView review_tv = (TextView) findViewById(R.id.review_tv);
+        
+        NetworkImageView image_view = (NetworkImageView) findViewById(R.id.image_view);
+
+		// write content
+		name_tv.setText(selected_name);
+		address_tv.setText(found_kneipe.getStreet() + ", " + found_kneipe.getCity());
+		rating_tv.setText("Rating: " + found_kneipe.getRating());
+		review_tv.setText("Review: " + found_kneipe.getReview());
+		
+		// load image
+		RequestQueue mRequestQueue = Volley.newRequestQueue(getBaseContext());
+		ImageLoader mImageLoader = new ImageLoader(mRequestQueue, new ImageLoader.ImageCache() {
+		    private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
+			@Override
+		    public void putBitmap(String url, Bitmap bitmap) {
+		        mCache.put(url, bitmap);
+		    }
+		    public Bitmap getBitmap(String url) {
+		        return mCache.get(url);
+		    }
+		});
+
+		image_view.setImageUrl("http://rootwiki.net/wp-content/uploads/2013/10/Android-Happy2-e1357315066858.jpg", mImageLoader);
     }
 }
